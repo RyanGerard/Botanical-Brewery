0a3a26115c8be62f13630382b3bace5dd989dec9
Ryan Johnson <rgjohn22@gmail.com>
2023-01-18 16:49:07 -0600

    added smooth scroll to Beerlist

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+
 const toggleButton = document.getElementById('toggle-Button');
   const navList = document.getElementById('nav-list');
 
   toggleButton.addEventListener('click', () => {
     navList.classList.toggle('active');
   }) 
+
